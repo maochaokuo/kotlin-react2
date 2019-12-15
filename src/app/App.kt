c94fc2ea6027992bb9cae6848afe6e5e9afd3bcf
Thomas Mao <tm1813@gmail.com>
2019-12-15 17:09:31 +0800

    Using an external REST API 2

--- src/app/App.kt
+++ src/app/App.kt
@@ -1,6 +1,6 @@
 package app
 
-import kotlinx.coroutines.await
+import kotlinx.coroutines.*
 import react.*
 import react.dom.*
 import logo.*
@@ -18,22 +18,45 @@ interface AppState : RState {
 data class Video(val id: Int, val title: String, val speaker: String, val videoUrl: String)
 
 class App: RComponent<RProps, AppState>() {
-    suspend fun fetchVideo(id: Int): Video {
-        val responsePromise = window.fetch("https://my-json-server.typicode.com/kotlin-hands-on/kotlinconf-json/videos/$id")
-        val response = responsePromise.await()
-        val jsonPromise = response.json()
-        val json = jsonPromise.await()
-        return json.unsafeCast<Video>()
+    suspend fun fetchVideos(): List<Video> = coroutineScope {
+        (1..25).map { id ->
+            async {
+                fetchVideo(id)
+            }
+        }.awaitAll()
     }
+    suspend fun fetchVideo(id: Int): Video =
+        window.fetch("https://my-json-server.typicode.com/kotlin-hands-on/kotlinconf-json/videos/$id")
+            .await()
+            .json()
+            .await()
+            .unsafeCast<Video>()
+//    suspend fun fetchVideo(id: Int): Video {
+//        val responsePromise = window.fetch("https://my-json-server.typicode.com/kotlin-hands-on/kotlinconf-json/videos/$id")
+//        val response = responsePromise.await()
+//        val jsonPromise = response.json()
+//        val json = jsonPromise.await()
+//        return json.unsafeCast<Video>()
+//    }
     override fun AppState.init() {
-        unwatchedVideos = listOf(
-                Video(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
-                Video(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
-                Video(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
-        )
-        watchedVideos = listOf(
-                Video(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
-        )
+    unwatchedVideos = listOf()
+    watchedVideos = listOf()
+
+    val mainScope = MainScope()
+    mainScope.launch {
+        val videos = fetchVideos()
+        setState {
+            unwatchedVideos = videos
+        }
+    }
+//        unwatchedVideos = listOf(
+//                Video(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
+//                Video(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
+//                Video(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
+//        )
+//        watchedVideos = listOf(
+//                Video(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
+//        )
     }
 
     override fun RBuilder.render() {

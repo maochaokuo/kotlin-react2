71e4c2e6c8f373bcf2d6dcf2509666789b5ac3c0
Thomas Mao <tm1813@gmail.com>
2019-12-15 10:03:15 +0800

    A first static page with React 2

--- src/app/App.kt
+++ src/app/App.kt
@@ -7,10 +7,41 @@ import ticker.*
 
 
 class App : RComponent<RProps, RState>() {
-    override fun RBuilder.render() {
-        h1 {
-            +"Hello, React+Kotlin/JS!"
+    override fun RBuilder.render() {h1 {
+        +"KotlinConf Explorer"
+    }
+        div {
+            h3 {
+                +"Videos to watch"
+            }
+            p {
+                +"John Doe: Building and breaking things"
+            }
+            p {
+                +"Jane Smith: The development process"
+            }
+            p {
+                +"Matt Miller: The Web 7.0"
+            }
+
+            h3 {
+                +"Videos watched"
+            }
+            p {
+                +"Tom Jerry: Mouseless development"
+            }
         }
+        div {
+            h3 {
+                +"John Doe: Building and breaking things"
+            }
+            img {
+                attrs {
+                    src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+                }
+            }
+        }
+
 //        div("App-header") {
 //            logo()
 //            h2 {
